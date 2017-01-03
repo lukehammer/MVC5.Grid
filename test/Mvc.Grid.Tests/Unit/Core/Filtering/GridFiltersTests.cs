f9f9c11749983372f11f88a7568d8fb7d6c354bf
Muchiachio <aldis.rameika@gmail.com>
2017-01-03 19:31:26 +0200

    Code base clean up
    - Cleaned up code base.

--- test/Mvc.Grid.Tests/Unit/Core/Filtering/GridFiltersTests.cs
+++ test/Mvc.Grid.Tests/Unit/Core/Filtering/GridFiltersTests.cs
@@ -285,7 +285,7 @@ namespace NonFactors.Mvc.Grid.Tests.Unit
         public void Register_FilterForExistingType()
         {
             Dictionary<String, Type> filter = new Dictionary<String, Type> { { "Test", typeof(Object) } };
-            filters.Table.Add(typeof(Object), filter);
+            filters.Table[typeof(Object)] = filter;
 
             filters.Register(typeof(Object), "TestFilter", typeof(String));
 
@@ -299,9 +299,7 @@ namespace NonFactors.Mvc.Grid.Tests.Unit
         public void Register_NullableFilterTypeForExistingType()
         {
             Dictionary<String, Type> filter = new Dictionary<String, Type> { { "Test", typeof(Object) } };
-
-            filters.Table.Remove(typeof(Int32));
-            filters.Table.Add(typeof(Int32), filter);
+            filters.Table[typeof(Int32)] = filter;
 
             filters.Register(typeof(Int32?), "TestFilter", typeof(String));
 
@@ -315,9 +313,7 @@ namespace NonFactors.Mvc.Grid.Tests.Unit
         public void Register_Overrides_NullableFilter()
         {
             Dictionary<String, Type> filter = new Dictionary<String, Type> { { "Test", typeof(Object) } };
-
-            filters.Table.Remove(typeof(Int32));
-            filters.Table.Add(typeof(Int32), filter);
+            filters.Table[typeof(Int32)] = filter;
 
             filters.Register(typeof(Int32?), "Test", typeof(String));
 
@@ -331,9 +327,7 @@ namespace NonFactors.Mvc.Grid.Tests.Unit
         public void Register_Overrides_Filter()
         {
             Dictionary<String, Type> filter = new Dictionary<String, Type> { { "Test", typeof(Object) } };
-
-            filters.Table.Remove(typeof(Int32));
-            filters.Table.Add(typeof(Int32), filter);
+            filters.Table[typeof(Int32)] = filter;
 
             filters.Register(typeof(Int32), "Test", typeof(String));
 
@@ -373,7 +367,7 @@ namespace NonFactors.Mvc.Grid.Tests.Unit
         public void Unregister_ExistingFilter()
         {
             Dictionary<String, Type> filter = new Dictionary<String, Type> { { "Test", null } };
-            filters.Table.Add(typeof(Object), filter);
+            filters.Table[typeof(Object)] = filter;
 
             filters.Unregister(typeof(Object), "Test");
 
