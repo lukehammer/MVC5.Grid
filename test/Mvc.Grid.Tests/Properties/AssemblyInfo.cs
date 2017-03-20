dc871cba334e00fdc98c4ac5d6147ebf1c9421eb
Muchiachio <aldis.rameika@gmail.com>
2017-03-20 18:01:31 +0200

    Project tooling migration
    - Migrated old csproj project format tooling to the new csproj one.

--- test/Mvc.Grid.Tests/Properties/AssemblyInfo.cs
+++ test/Mvc.Grid.Tests/Properties/AssemblyInfo.cs
@@ -1,14 +1,3 @@
-﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.InteropServices;
-using Xunit;
+﻿using Xunit;
 
-[assembly: ComVisible(false)]
-[assembly: AssemblyTitle("Mvc.Grid.Tests")]
-[assembly: AssemblyProduct("Mvc.Grid.Tests")]
-[assembly: AssemblyCompany("NonFactors")]
-[assembly: AssemblyCopyright("Copyright © NonFactors")]
-[assembly: NeutralResourcesLanguage("en")]
-[assembly: AssemblyFileVersion("3.4.0.0")]
-[assembly: AssemblyVersion("3.4.0.0")]
 [assembly: CollectionBehavior(DisableTestParallelization = true)]
